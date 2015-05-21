ece3ec4dea7c45b604193330397e774fa1ff5612
Francesco Gatti <gattifrancesco@hotmail.it>
2015-05-21 23:11:17 +0200

    added github-search setup

---
+++ github-search/README.md
@@ -0,0 +1,6 @@
+#INSTALL:
+sudo python2 setup.py install
+
+#USAGE:
+github-search <name_to_search>
+"-h" for more help
---
+++ github-search/setup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python2
+from distutils.core import setup
+
+setup(
+	name='github-search',
+	scripts = ['github-search.py'],
+)
